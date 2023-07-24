bc8e5173747a582bd096c8a111e2980359e296bf
pichet <98628678+pichetx@users.noreply.github.com>
2023-07-24 23:33:57 +0100

    Update edit.py

--- mobile-mining/edit.py
+++ mobile-mining/edit.py
@@ -92,20 +92,30 @@ def OffMiner():
 
     banner()
     try:
-        print("ชื่อ pool เช่น \033[93mstratum+tcp://ap.luckpool.net:3956\033[00m")
-        pool = input("[-o]: ")
-
-        print("เลขกระเป๋า เช่น \033[93mRKh6cinBtWFspyBfK6Xsu8JKJsFyfYmUCr\033[00m")
-        wallet = input("[-u]: ")
+       print("ตัวอย่าง:")
+        print(" \033[93mstratum+tcp://ap.luckpool.net:3956\033[00m")
+        print(" \033[93mstratum+tcp://verus.asia.mine.zergpool.com:3300\033[00m")
+        pool = input(" pool : ")
+        print("\033[35m-----------------------------------------\033[0m")
+        
+      
+        print("ตัวอย่าง: \033[93mRQpWNdNZ4LQ5yHUM3VAVuhUmMMiMuGLUhT\033[00m")
+        wallet = input("wallet: ")
+        print("\033[35m-----------------------------------------\033[0m")
 
         print("ชื่อคนงานขุด เช่น \033[93mMiner01\033[00m")
         name = input("[-n]: ")
-
-        print("Password เช่น \033[93mx หรือ hybrid ( เฉพาะ luckpool )\033[00m")
-        password = input("[-p]: ")
+        print("\033[35m-----------------------------------------\033[0m")
+        
+        print("ตัวอย่าง:")
+        print("  \033[93mx หรือ ( hybrid เฉพาะ luckpool )\033[00m")
+        print("  \033[93mc=DOGE,mc=VRSC (ไม่ต้องใส่ id=ชื่อ ระบบจะเพิ่มให้เอง)\033[00m")
+        password = input("password : ")
+        print("\033[35m-----------------------------------------\033[0m")
 
         print(f"จำนวนthread \033[93mค่าที่ใส่ได้คือ 0 ถึง {cpu_thread}\033[00m")
         cpu = int(input("[-t]: "))
+        print("\033[35m-----------------------------------------\033[
         
         if pool == "" or wallet == "":
             raise Exception()
