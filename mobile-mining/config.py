4070022edd95c63be6c9dbc008d80f3a0d827e17
pichet <98628678+pichetx@users.noreply.github.com>
2023-07-25 03:00:55 +0100

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -21,7 +21,7 @@ def banner():
     print("\033[1;34;40m")
     os.system("figlet -f big VERUS")
     print("\033[00m\n")
-    print("\033[36mEdit by PICHET SAENGTEWAN\033[0m")
+    print("\033[35mEdit by PICHET SAENGTEWAN\033[0m")
     print("\033[35mDebug Colors by AMARIN PRAPRIWIT\033[0m")
     print("\033[36m\033[0m")
     if active == "on":
