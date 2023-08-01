6656c30fda8ff36fdd05a1ea70f9cf11a53d1dab
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-01 21:27:25 +0100

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -1,22 +1,7 @@
 import os, json, requests
 
 
-
-
-def versionApp():
-    with open("version.json", encoding="utf-8") as read:
-        load = read.read()
-        loads = json.loads(load)
-        return loads['version']
-
-# banner function
 def banner():
-    with open("active.json", encoding="utf-8") as read:
-        load = read.read()
-        loads = json.loads(load)
-        active = loads['status']
-        
-
     os.system("clear")
     print("\033[1;34;40m")
     os.system("figlet -f big VERUS")
