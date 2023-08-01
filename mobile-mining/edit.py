4785b199ed04fac72935bee31ff16444cbf94669
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-01 20:22:16 +0700

    Update edit.py

--- mobile-mining/edit.py
+++ mobile-mining/edit.py
@@ -20,14 +20,16 @@ def OffMiner():
        wallet = input("wallet: ")
        print("\033[35m-----------------------------------------\033[0m")
         
-       print("ชื่อคนงานขุด เช่น \033[93mMiner01\033[00m")
-       name = input("[-n]: ")
-       print("\033[35m-----------------------------------------\033[0m")
-        
        print("ตัวอย่าง:")
        print("  \033[93mx หรือ ( hybrid เฉพาะ luckpool )\033[00m")
        print("  \033[93mc=DOGE,mc=VRSC (ไม่ต้องใส่ id=ชื่อ ระบบจะเพิ่มให้เอง)\033[00m")
        password = input("password : ")
+
+
+print("\033[35m-----------------------------------------\033[0m")
+       
+       print("ชื่อคนงานขุด เช่น \033[93mMiner01\033[00m")
+       name = input("[-n]: ")
        print("\033[35m-----------------------------------------\033[0m")
         
        print(f"จำนวนthread \033[93mค่าที่ใส่ได้คือ 0 ถึง {cpu_thread}\033[00m")
@@ -36,10 +38,10 @@ def OffMiner():
         
        if pool == "" or wallet == "":
             raise Exception()
-       if name == "":
-            raise Exception()
        if password == "":
             password = "x"
+       if name == "":
+            raise Exception()
        if cpu == "":
             cpu = 1
     except:
