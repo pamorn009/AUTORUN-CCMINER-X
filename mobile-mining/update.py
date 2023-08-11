f8fd371faf4200de1695c6815af6bcad37031fbe
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-11 03:44:11 +0100

    Update update.py

--- mobile-mining/update.py
+++ mobile-mining/update.py
@@ -6,14 +6,14 @@ from config import bannerup
 try:
     os.system("@cls||clear")
     bannerup()
-    os.system(f"cd && rm -rf AUTORUN-CCMINER-X && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../ && cd ../etc && rm -rf mobile-mining")
-    os.system(f"cd && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../")
-    os.system(f"cd && cd ../etc && rm -rf mobile-mining")
-    os.system(f"cd && cd ../bin && rm -f edit-miner run-miner add-ip")
-    os.system(f"cd && git clone https://github.com/pichetx/AUTORUN-CCMINER-X")
-    os.system(f"cd && cd AUTORUN-CCMINER-X/mobile-mining/set-miner && rm -r offline.json online.json")
-    os.system(f"cd && mv offline.json online.json /AUTORUN-CCMINER-X/mobile-mining/set-miner")
-    os.system(f"cd && cd AUTORUN-CCMINER-X && chmod +x setupdate.sh && sh setupdate.sh")
+    os.system("cd && rm -rf AUTORUN-CCMINER-X && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../ && cd ../etc && rm -rf mobile-mining")
+    os.system("cd && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../")
+    os.system("cd && cd ../etc && rm -rf mobile-mining")
+    os.system("cd && cd ../bin && rm -f edit-miner run-miner add-ip")
+    os.system("cd && git clone https://github.com/pichetx/AUTORUN-CCMINER-X")
+    os.system("cd && cd AUTORUN-CCMINER-X/mobile-mining/set-miner && rm -r offline.json online.json")
+    os.system("cd && mv offline.json online.json /AUTORUN-CCMINER-X/mobile-mining/set-miner")
+    os.system("cd && cd AUTORUN-CCMINER-X && chmod +x setupdate.sh && sh setupdate.sh")
     
     time.sleep(2)
 except:
