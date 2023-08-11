528d37bb296500ce9a809ce44843fe7ae09035db
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-11 03:16:58 +0100

    Update update.py

--- mobile-mining/update.py
+++ mobile-mining/update.py
@@ -7,9 +7,11 @@ try:
     os.system("@cls||clear")
     bannerup()
     os.system(f"cd && rm -rf AUTORUN-CCMINER-X && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../ && cd ../etc && rm -rf mobile-mining")
+    os.system(f"cd && cd ../etc/mobile-mining/set-miner && cp -r offline.json online.json /../")
+    os.system(f"cd && cd ../etc && rm -rf mobile-mining")
     os.system(f"cd && cd ../bin && rm -f edit-miner run-miner add-ip")
     os.system(f"cd && git clone https://github.com/pichetx/AUTORUN-CCMINER-X")
-    os.system(f"cd set-miner && rm -r offline.json online.json")
+    os.system(f"cd && cd AUTORUN-CCMINER-X/mobile-mining/set-miner && rm -r offline.json online.json")
     os.system(f"cd && mv offline.json online.json /AUTORUN-CCMINER-X/mobile-mining/set-miner")
     os.system(f"cd && cd AUTORUN-CCMINER-X && chmod +x setupate.sh && sh setupdate.sh")
     
