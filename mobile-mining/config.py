3036c0a1156b44acbba8be255ab2361d0498d9fc
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-21 02:21:09 +0700

    Update config.py

--- mobile-mining/config.py
+++ mobile-mining/config.py
@@ -19,3 +19,10 @@ def bannerup():
     os.system("figlet -f big UPDATE")
     print("\033[00m\n")
     print(f"\n\033[96mกำลัง UPDATE Miner\033[0m\n")
+
+def banneredit():
+    os.system("clear")
+    print("\033[1;34;34m")
+    os.system("figlet -f big EDIT")
+    print("\033[00m\n")
+    print(f"\n\033[96mการตั้งค่า Miner\033[0m\n")
