d70fd32edcd701e1ee2119b8cb0cf9da907952ad
pichet <98628678+pichetx@users.noreply.github.com>
2023-08-24 21:59:16 +0100

    Update update.py

--- mobile-mining/update.py
+++ mobile-mining/update.py
@@ -1,1 +1,10 @@
+import schedule
+import time
+import os
+from config import bannerup
 
+
+def updatedata():
+    os.system(f"update")
+#schedule.every().day.at('05:00').do(updatedata)
+schedule.every(3000).second.do(updatedata)
